06375dc9473d41365d451fb6b7c2beb94f07d74b
koda5 <lkodovski@gmail.com>
2021-12-10 17:04:23 +0100

    register fix

--- client/src/App.js
+++ client/src/App.js
@@ -26,6 +26,7 @@ const App = () => {
                 <Routes>
                   <Route path='/' element={<PrivateRoute component={Home} />} />
                   <Route path='about' element={<About />} />  
+                  <Route path='register' element={<Register />} />  
                   <Route path='login' element={<Login />} />
                 </Routes>
               </div>
